893359dd05c67bdc645bb32c44258b85cd94205a
PIAOZHUZI <atig00328@naver.com>
2019-02-09 13:12:34 +0900

    TypeCastExample

---
+++ TypeCastExample/TypeCastExample/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeCastExample
+{
+    class TypeCastExample1
+    {
+        public static void Main()
+        {
+            int a = 50000;
+            try
+            {
+                short b = checked((short)a); // overflow를 검사하여 예외를 발생시킴
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine("예외: { 0}",e.StackTrace);
+            }
+        }
+    }
+}
